01f666bc0326b8f220f2f4d53c202657f46aeae4
sirius <zznzhang@ucdavis.edu>
2017-10-27 10:31:44 -0700

    class wraper

--- C3PO/traffic_monitoring.py
+++ C3PO/traffic_monitoring.py
@@ -7,16 +7,36 @@ import os
 
 # Todo: display labels and confidence level
 
+class Object:
+    def __init__(self, detection_output, frame_seq = 0):
+        self.label = self.extract_label(detection_output)
+        self.box_top_left, self.box_bottom_right = self.extract_box_corners(detection_output)
+        self.confidence = self.extract_confidence(detection_output)
+        self.frame_seq = frame_seq
+
+    def extract_label(self, detection_output):
+        return detection_output[0]
+
+    def extract_confidence(self,detection_output):
+        return detection_output[1]
+
+    def extract_box_corners(self, detection_output):
+        x = detection_output[2][0]
+        y = detection_output[2][1]
+        w = detection_output[2][2]
+        h = detection_output[2][3]
+        return (int(x - w/2),int(y - h/2)), (int(x + w/2),int(y + h/2))
+
 def pipeline(frame):
     return frame
 
-def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+def draw_boxes(img, object, color=(0, 0, 255), thick=6):
     # Make a copy of the image
     imcopy = np.copy(img)
 
     # Draw a rectangle given bbox coordinates
-    left_up_corner = (int(bboxes[0] - bboxes[2]/2),int(bboxes[1] - bboxes[3]/2))
-    bottom_right_corner = (int(bboxes[0] + bboxes[2]/2),int(bboxes[1] + bboxes[3]/2))
+    left_up_corner = object.box_top_left
+    bottom_right_corner = object.box_bottom_right
     cv2.rectangle(imcopy, left_up_corner, bottom_right_corner, color, thick)
 
     # Return the image copy with boxes drawn
@@ -39,7 +59,8 @@ if __name__ == "__main__":
     #im = cv2.imread("data/dog.jpg")
     im = mpimg.imread("darknet/data/dog.jpg")
     for detection in r:
-        im = draw_boxes(im, detection[2])
+        ob = Object(detection)
+        im = draw_boxes(im, ob)
     
     plt.imshow(im)
     plt.show()
